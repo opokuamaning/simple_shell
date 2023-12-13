8a4882c5715615ec43566b891e3d8f1b4454a24c
opokuamaning <princeopokuamaning@gmail.com>
2023-12-13 06:26:54 -0800

    Made changes t shell.j

--- shell.h
+++ shell.h
@@ -2,18 +2,39 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
-#define BUFFER_SIZE 1024
-#define PROMPT "#cisfun$ "
+typedef struct {
+    char *name;
+    char *value;
+} Alias;
 
-ssize_t read_line(char *buffer, size_t size);
-void execute_command(char *command, char *args[]);
+#define MAX_ALIASES 100
+
+#define ARG_MAX 1024
+
+void setup_environment(void);
+char *get_input(void);
+void parse_input(char *input);
+void cleanup_memory(void);
+void execute_single_command(char *command);
+void execute_command(char *input);
+void execute_commands_from_file(const char *filename);
+void handle_exit(void);
+void handle_env(void);
+char *custom_getline(void);
+void tokenize_input(char *input, char *args[], int *arg_count);
+int is_valid_integer(const char *str);
+void handle_setenv(char *variable, char *value);
+void handle_unsetenv(char *variable);
+void change_directory(char *path);
+void handle_cd(char *directory);
+void handle_alias(int arg_count, char *args[]);
+char *replace_variables(char *command);
+void tokenize_input(const char *input, char *args[], int *arg_count);
 
 #endif
