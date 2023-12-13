2d54c3e625fe1f3a8121a99f3f6fa5114044cb71
opokuamaning <princeopokuamaning@gmail.com>
2023-12-13 12:32:47 -0800

    Added shell.h

--- shell.h
+++ shell.h
@@ -1,40 +1,63 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SHELL
+#define SHELL
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
-#include <ctype.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
-typedef struct {
-    char *name;
-    char *value;
-} Alias;
+/**
+ * struct list - linked list for environmental variables
+ * @var: holds environmental variable string
+ * @next: points to next node
+ */
+typedef struct list
+{
+	char *var;
+	struct list *next;
 
-#define MAX_ALIASES 100
+} list_t;
 
-#define ARG_MAX 1024
-
-void setup_environment(void);
-char *get_input(void);
-void parse_input(char *input);
-void cleanup_memory(void);
-void execute_single_command(char *command);
-void execute_command(char *input);
-void execute_commands_from_file(const char *filename);
-void handle_exit(void);
-void handle_env(void);
-char *custom_getline(void);
-void tokenize_input(char *input, char *args[], int *arg_count);
-int is_valid_integer(const char *str);
-void handle_setenv(char *variable, char *value);
-void handle_unsetenv(char *variable);
-void change_directory(char *path);
-void handle_cd(char *directory);
-void handle_alias(int arg_count, char *args[]);
-char *replace_variables(char *command);
-void tokenize_input(const char *input, char *args[], int *arg_count);
+/* function prototypes */
+int prompt(char **env);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+size_t get_line(char **str);
+int t_strlen(char *str, int pos, char delm);
+char *ignore_space(char *str);
+char **_str_tok(char *str, char *delm);
+char **c_str_tok(char *str, char *delm);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _cd(char **str, list_t *env, int num);
+int built_in(char **token, list_t *env, int num, char **command);
+void non_interactive(list_t *env);
+char *_which(char *str, list_t *env);
+int __exit(char **s, list_t *env, int num, char **command);
+int _execve(char *argv[], list_t *env, int num);
+void free_double_ptr(char **str);
+void free_linked_list(list_t *list);
+int _env(char **str, list_t *env);
+char *get_env(char *str, list_t *env);
+list_t *env_linked_list(char **env);
+list_t *add_end_node(list_t **head, char *str);
+size_t print_list(list_t *h);
+int delete_nodeint_at_index(list_t **head, int index);
+int _unsetenv(list_t **env, char **str);
+int _setenv(list_t **env, char **str);
+int find_env(list_t *env, char *str);
+void not_found(char *str, int num, list_t *env);
+void cant_cd_to(char *str, int c_n, list_t *env);
+void illegal_number(char *str, int c_n, list_t *env);
+char *int_to_string(int num);
 
 #endif
+
