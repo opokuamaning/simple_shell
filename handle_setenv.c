f90bfe8bc125ca23eb5a1ea192c0c0516598d13b
Leslie <leslieasante44@gmail.com>
2023-12-13 08:35:18 -0800

    debugging handle setenv

--- handle_setenv.c
+++ handle_setenv.c
@@ -1,23 +1,28 @@
 #include <shell.h>
-
 /**
- * Handle the setenv built-in command.
+ * handle_setenv - Handle the 'setenv' built-in command.
  *
- * @param variable The name of the environment variable.
- * @param value The value to set for the environment variable.
+ * @variable: The name of the environment variable to set.
+ * @value: The value to assign to the environment variable.
+ *
+ * Description:
+ * This function handles the 'setenv' command, which sets or updates
+ * the value of an environment variable. It checks if the variable name
+ * is valid before attempting to set the environment variable. If successful,
+ * the environment variable is set or updated; otherwise, an error message
+ * is printed to stderr.
  */
-
 void handle_setenv(char *variable, char *value)
 {
-    if (variable != NULL && is_valid_variable(variable))
-    {
-        if (setenv(variable, value, 1) == -1)
-        {
-            perror("Error setting environment variable");
-        }
-    }
-    else
-    {
-        fprintf(stderr."setenv: Invalid variable name\n");
-    }
+	if (variable != NULL && is_valid_variable(variable))
+	{
+		if (setenv(variable, value, 1) == -1)
+		{
+			perror("Error setting environment variable");
+		}
+	}
+	else
+	{
+		fprintf(stderr."setenv: Invalid variable name\n");
+	}
 }
