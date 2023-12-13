b08f475892e8e812277890191d748e64ce343183
opokuamaning <princeopokuamaning@gmail.com>
2023-12-13 08:39:47 -0800

    check for betty compliance on handle unsetenv

--- handle_unsetenv.c
+++ handle_unsetenv.c
@@ -1,23 +1,26 @@
 #include <shell.h>
-
 /**
- * Handle the setenv built-in command.
+ * handle_unsetenv - Handle the 'unsetenv' built-in command.
  *
- * @param variable The name of the environment variable.
- * @param value The value to set for the environment variable.
+ * @variable: The name of the environment variable to unset.
+ *
+ * Description:
+ * This function handles the 'unsetenv' command, which removes the specified
+ * environment variable. It checks if the variable name is valid before
+ * attempting to unset the environment variable. If successful, the environment
+ * variable is removed; otherwise, an error message is printed to stderr.
  */
-
-void handle_setenv(char *variable, char *value)
+void handle_unsetenv(char *variable)
 {
-    if (variable != NULL && is_valid_variable(variable))
-    {
-        if (setenv(variable, value, 1) == -1)
-        {
-            perror("Error setting environment variable");
-        }
-    }
-    else
-    {
-        fprintf(stderr."setenv: Invalid variable name\n");
-    }
+	if (variable != NULL && is_valid_variable(variable))
+	{
+		if (unsetenv(variable) == -1)
+		{
+			perror("Error unsetting envrionmanet variable");
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Unsetenv: Invalid variable name\n");
+	}
 }
