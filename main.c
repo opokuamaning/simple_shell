753943c26be1cecf0615817c3eb470e899b249b5
opokuamaning <princeopokuamaning@gmail.com>
2023-12-13 06:17:14 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ *  main - Entry point for the shell
+ *  @argc: argument count
+ *  @arrgv: argument vector
+ *  @env: environment variables
+ *  Return: Exit status
+ */
+int main(int argc, char *argv[], char *env[])
+{
+	(void)argc;
+	(void)argv;
+	(void)env;
+
+	return (EXIT_SUCCESS);
+}
--- shell.h
+++ shell.h
@@ -3,5 +3,17 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+#define PROMPT "#cisfun$ "
+
+ssize_t read_line(char *buffer, size_t size);
+void execute_command(char *command, char *args[]);
 
 #endif
