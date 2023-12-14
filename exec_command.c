a4f3162af93fabc29220dfd800b949e26d61c909
Prince Opoku Amaning <105636490+opokuamaning@users.noreply.github.com>
2023-12-14 18:32:40 +0000

    Update exec_command.c

--- exec_command.c
+++ exec_command.c
@@ -13,97 +13,98 @@
  */
 int execute_single_command(char *command)
 {
-	char *modified_command = replace_variables(command);
+    char *modified_command = replace_variables(command);
 
-	pid_t pid;
-	int status;
+    pid_t pid;
+    int status;
 
-	int arg_count;
-	char *args[ARG_MAX];
+    int arg_count;
+    char *args[ARG_MAX];
 
-	tokenize_input(modified_command, args, &arg_count);
+    tokenize_input(modified_command, args, &arg_count);
 
-	if (arg_count > 0 && strcmp(args[0], "exit") == 0)
-	{
-		if (arg_count > 1)
-		{
-			handle_exit(args[1]);
-		}
-		else
-		{
-			handle_exit(NULL);
-		}
-	}
-	else if (arg_count > 0 && strcmp(args[0], "env") == 0)
-	{
-		handle_env();
-	}
-	else if (arg_count > 2 && strcmp(args[0], "setenv") == 0)
-	{
-		handle_setenv(args[1], args[2]);
-	}
-	else if (arg_count > 1 && strcmp(args[0], "unsetenv") == 0)
-	{
-		handle_unsetenv(args[1]);
-	}
-	else if (arg_count > 0 && strcmp(args[0], "cd") == 0)
-	{
-		handle_cd(arg_count > 1 ? args[1] : NULL);
-	}
-	else
-	{
-		pid = fork();
+    if (arg_count > 0 && strcmp(args[0], "exit") == 0)
+    {
+        if (arg_count > 1)
+        {
+            handle_exit(args[1]);
+        }
+        else
+        {
+            handle_exit(NULL);
+        }
+    }
+    else if (arg_count > 0 && strcmp(args[0], "env") == 0)
+    {
+        handle_env();
+    }
+    else if (arg_count > 2 && strcmp(args[0], "setenv") == 0)
+    {
+        handle_setenv(args[1], args[2]);
+    }
+    else if (arg_count > 1 && strcmp(args[0], "unsetenv") == 0)
+    {
+        handle_unsetenv(args[1]);
+    }
+    else if (arg_count > 0 && strcmp(args[0], "cd") == 0)
+    {
+        handle_cd(arg_count > 1 ? args[1] : NULL);
+    }
+    else
+    {
+        pid = fork();
 
-		if (pid == -1)
-		{
-			perror("Error forking process");
-			return (-1);
-		}
+        if (pid == -1)
+        {
+            perror("Error forking process");
+            return (-1);
+        }
 
-		if (pid == 0)
-		{
-			args[arg_count] = NULL;
+        if (pid == 0)
+        {
+            args[arg_count] = NULL;
 
-			if (execvp(args[0], args) == -1)
-			{
-				perror("Error executing command");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			siginfo_t info;
-			waitid(P_PID, pid, &info, WEXITED, NULL);
+            if (execvp(args[0], args) == -1)
+            {
+                perror("Error executing command");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
 
-			if (strstr(command, "&&"))
-			{
-				if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
-				{
-					char *next_command = strchr(command, '&') + 2;
+            if (strstr(command, "&&"))
+            {
+                if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+                {
+                    char *next_command = strchr(command, '&') + 2;
 
-					if (next_command != NULL)
-					{
-						execute_single_command(next_command);
-					}
-				}
-			}
-			else if (strstr(command, "||"))
-			{
-				if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-				{
-					char *next_command = strchr(command, '&') + 2;
+                    if (next_command != NULL)
+                    {
+                        execute_single_command(next_command);
+                    }
+                }
+            }
+            else if (strstr(command, "||"))
+            {
+                if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+                {
+                    char *next_command = strchr(command, '&') + 2;
 
-					if (next_command != NULL)
-					{
-						execute_single_command(next_command);
-					}
-				}
-			}
-		}
-	}
-	free(modified_command);
-	return (0);
+                    if (next_command != NULL)
+                    {
+                        execute_single_command(next_command);
+                    }
+                }
+            }
+        }
+    }
+
+    free(modified_command);
+    return status;
 }
+
 /**
  * execute_commands_from_file - Execute commands from a file.
  *
