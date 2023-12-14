c779c0983122e1d99ac3c388251e2acbcf81a354
Leslie <leslieasante44@gmail.com>
2023-12-14 05:15:01 -0800

    debugging exec command

--- execute_command.c
+++ execute_command.c
@@ -16,30 +16,36 @@ int execute_command(char *command)
 	args[1] = NULL;
 
 	pid = fork();
+	/*Check if the command exists in the PATH*/
+	if (access(command, X_OK) == -1)
+	{
+		/*Command not found, print an error message and return*/
+		perror("Command not found");
+		return (1);
+	}
+
+	pid_t pid = fork();
 
 	if (pid == 0)
 	{
-		/* Child process */
-		if (execve(args[0], args, NULL) == -1)
+		/* Child process*/
+		if (execve(command, args, NULL) == -1)
 		{
 			perror("Error in child process");
 			exit(EXIT_FAILURE);
 		}
-	}
-	else if (pid < 0)
+	} else if (pid < 0)
 	{
-		/* Forking error */
+		/*Forking error*/
 		perror("Error forking process");
 		exit(EXIT_FAILURE);
-	}
-	else
+	} else
 	{
-		/* Parent process */
+		/* Parent process*/
 		do {
 			pid = waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
-
 	return (0);
 }
 
